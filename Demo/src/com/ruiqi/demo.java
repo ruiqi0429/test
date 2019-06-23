3541088b8698c03ca2b42be65eb7a13cdae19560
ruiqi0429 <ruiqi0429@foxmail.com>
2019-06-23 19:23:50 +0800

    使用ieda提交

---
+++ Demo/src/com/ruiqi/demo.java
@@ -0,0 +1,13 @@
+package com.ruiqi;
+
+/**
+ * @author ：RuiQi
+ * @date ：Created in 2019/6/23
+ * @description ：
+ * @version: 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+    }
+}
